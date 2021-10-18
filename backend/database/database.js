21e8f7640511da296ed0b47a7c1829f3d58ad831
Hector Alvarez-Toledo <hect16@gmail.com>
2021-10-18 17:46:44 -0500

    created backend folder using express-genrator, connected to mongocloud db, and make an inital schema

---
+++ backend/database/database.js
@@ -0,0 +1,17 @@
+// const { MongoClient } = require("mongodb");
+
+// const connectionURL = "mongodb://127.0.0.1:27017";
+// const databaseName = "web-app";
+
+// MongoClient.connect(
+// 	connectionURL,
+// 	{ useUnifiedTopology: true },
+// 	(error, client) => {
+// 		if (error) {
+// 			return console.log("unable to connect to databse");
+// 		}
+// 		console.log("Successfully connected");
+
+// 		const db = client.db(databaseName);
+// 	}
+// );
